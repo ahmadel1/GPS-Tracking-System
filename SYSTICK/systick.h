e7397cb47a202148e30a19440b171e04383bee99
AmrBadra <125765171+AmrBadra@users.noreply.github.com>
2023-04-28 13:36:50 +0200

    Update systick.h

--- SYSTICK/systick.h
+++ SYSTICK/systick.h
@@ -1,7 +1,17 @@
-#ifndef __systick_H__
-#define __systick_H__
-#include "..\tm4c123gh6pm.h"
-#include <stdint.h>
+#ifndef systic_H_
+#define systic_H_
 
+#include "tm4c123gh6pm.h"
+#include "stdint.h"
+#include "string.h"
+
+//prototypes 
+void systick_init();
+void systick_wait_1s();
+void systick_wait_1ms();
+void delay_IN_ms(int total);
+void delay_IN_s(int total);
+void systick_wait_1MICROs();
+void delay_IN_MICROs(int total);
 
 #endif
