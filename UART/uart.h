942b136f629fc08e73a323c4fb93cb867f4af0b4
abdelrahman ali mohamed <131778595+abdelrahmanali15@users.noreply.github.com>
2023-04-28 04:14:43 +0200

    Update uart.h

--- UART/uart.h
+++ UART/uart.h
@@ -1,8 +1,20 @@
 #ifndef __uart_H__
 #define __uart_H__
 
+
+
+#include "tm4c123gh6pm.h"
 #include "stdint.h"
-#include "..\tm4c123gh6pm.h"
+#include "string.h"
+
+void UART1Init();
+void UART0Init();
+uint8_t UART1_AVAILABLE(void);
+uint8_t UART1_read(void);
+void UART1_write(uint8_t c);
+void UART1_readstr(uint8_t *str);
+void UART1_writestr(uint8_t *str);
+
 
 
 #endif
