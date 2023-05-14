f67becefa91b262fba10f536e7a32bf18b48d0c9
hazem-elgharabawy <117221359+hazem-elgharabawy@users.noreply.github.com>
2023-05-14 16:32:36 +0300

    Update gpio.h

---
+++ GPIO/gpio.h
@@ -0,0 +1,17 @@
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
+#include "..\tm4c123gh6pm.h"
+#include "stdint.h"
+#define RED    0x02
+#define BLUE   0x04
+#define GREEN  0x08
+#define YELLOW 0x0A
+
+
+void RGBInit (void);
+void RGBset (uint8_t color);
+void RGBclear(void);
+void currentState(float distance);
+
+#endif
